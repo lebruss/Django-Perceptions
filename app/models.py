3be1cf31e765ad065349a77da467fef90681c232
Caleb James Russell <caruss@ttu.ee>
2022-01-31 00:46:42 +0200

    Flavor text; created Person class; next I want to make the character creation page

--- app/models.py
+++ app/models.py
@@ -5,3 +5,15 @@ Definition of models.
 from django.db import models
 
 # Create your models here.
+
+#Each model is a Python class
+#   suclass django.db.models.Model
+#Each attribute of the model represents a data field
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+#https://docs.djangoproject.com/en/4.0/topics/db/models/
+
